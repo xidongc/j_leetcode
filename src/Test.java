1642a3c884c579a94402c2b653eca82765ed62a8
huijiang0102 <mengfei3@illinois.edu>
2018-10-09 23:23:52 -0500

    First Commit!

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class Test {
     public static void main(String[] a){
-        System.out.println("Start Program...");
+        System.out.println("Start Program...!");
         ArraySolution arr = new ArraySolution();
         int[] nums = new int[]{1};
         arr.findPeakElement(nums);
